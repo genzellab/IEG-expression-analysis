ffa3af14c2ab2884ca5c0b74ee878bfb964d754e
Amlra <115641807+Amlra@users.noreply.github.com>
2022-10-21 18:02:45 +0200

    Add files via upload

--- heatmapcode.m
+++ heatmapcode.m
@@ -8,24 +8,26 @@ load('namess.mat'); % numeric array with the brain regions names
 ax.XTickLabel = names;
 ax.YTickLabel = names;
 ax.XAxisLocation = 'top';
-title('Day 1', 'FontSize', 45);
+title('text', 'FontSize', 45);
 n = 50;                %// number of colors
 
 cmap(1,:) = [0 0 1];   %// color first row - blue
 cmap(2,:) = [1 1 1];   %// color 25th row - white
 cmap(3,:) = [1 0 0];   %// color 50th row - red
+shading interp;
 
-[X,Y] = meshgrid([1:3],[1:50]);  %// mesh of indices
+[X,Y] = meshgrid([1:3],[1:31]);  %// mesh of indices
 
-cmap = interp2(X([1,25,50],:),Y([1,25,50],:),cmap,X,Y); %// interpolate colormap
+cmap = interp2(X([1,16,31],:),Y([1,16,31],:),cmap,X,Y); %// interpolate colormap
 
-cmap(16:35,1) = 1;
-cmap(16:35,2) = 1;
-cmap(16:35,3) = 1;
+map1 = cmap(1:15,:);
+map2 = cmap(17:end,:); 
+new = ones(20,3);
+cmap = [map1;new;map2];
+
 colormap(cmap) %// set color map
 colorbar
 caxis([-1 1])
 colorbar ('eastoutside');
-
 
 
